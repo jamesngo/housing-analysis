76e04e8cc5adc54b4c020974eb637ad4f6c6c865
James Ngo <jngo@identitymind.com>
2019-06-30 23:07:17 -0700

    finished linear regression on additional columns

--- lin_reg.py
+++ lin_reg.py
@@ -4,27 +4,30 @@ import numpy as np
 import sys
 from sklearn.linear_model import LinearRegression
 
-combine= pd.read_csv("combine.csv")
-print(pd.read_csv("train.csv")['Id'])
-train = combine.loc[combine['Id'] in pd.read_csv("train.csv")['Id'], features]
-test = combine.loc[combine['Id'] in pd.read_csv("train.csv")['Id'], features]
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-
 features = ['LotFrontage','LotArea', 'OverallQual', 'OverallCond', 'YearRemodAdd', 'Foundation', 'YrSold']
 
+#splitting training and test sets
+combine= pd.read_csv("combine.csv")
+print(pd.read_csv("train.csv")['Id'])
+train = combine.loc[combine['Id'].isin(pd.read_csv("train.csv")['Id'].tolist())]
+test = combine.loc[combine['Id'].isin(pd.read_csv("test.csv")['Id'].tolist())]
 
+
+#splitting training set into target and predictors column using pandas dataframe constructor
 target = pd.DataFrame(data=train, columns=['SalePrice'], copy=True)
 predictors = pd.DataFrame(data=train, columns=features, copy=True)
 
-
+#fitting the data
 reg = LinearRegression().fit(predictors, target)
 
+#creating new dataframe which will have our result in it, we will index it using "Id"
 res = pd.DataFrame()
 res['Id'] = test['Id']
 
+
+#prediction
 res['SalePrice'] = reg.predict(test[features])
 
 
+#export to csv
 res.to_csv("res.csv", index=False)

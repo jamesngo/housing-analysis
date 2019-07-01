049e9ecfa567e85d66dbea63c54427deec1747f4
Sophie Jiang <sophiejiang@Sophies-MacBook-Pro.local>
2019-06-30 22:48:24 -0700

    added more columns to linear regression

--- lin_reg.py
+++ lin_reg.py
@@ -4,18 +4,15 @@ import numpy as np
 import sys
 from sklearn.linear_model import LinearRegression
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+combine= pd.read_csv("combine.csv")
+print(pd.read_csv("train.csv")['Id'])
+train = combine.loc[combine['Id'] in pd.read_csv("train.csv")['Id'], features]
+test = combine.loc[combine['Id'] in pd.read_csv("train.csv")['Id'], features]
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-features = ['LotFrontage','LotArea', 'OverallQual', 'OverallCond', 'YearRemodAdd', 'GarageArea']
-
-#imputing with mean
-train['LotFrontage'].fillna(train['LotFrontage'].mean(), inplace=True)
-test['LotFrontage'].fillna(test['LotFrontage'].mean(), inplace=True)
-test['BsmtFinSF1'].fillna(test['BsmtFinSF1'].mean(), inplace=True)
+features = ['LotFrontage','LotArea', 'OverallQual', 'OverallCond', 'YearRemodAdd', 'Foundation', 'YrSold']
 
 
 target = pd.DataFrame(data=train, columns=['SalePrice'], copy=True)

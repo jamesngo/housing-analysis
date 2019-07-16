4d7be562c7924a05a958515d3a18b703b70e93ed
James Ngo <jngo@identitymind.com>
2019-07-15 21:29:52 -0700

    modified gbm model to allow cv testing

--- gbm_reg.py
+++ gbm_reg.py
@@ -3,24 +3,36 @@ import pandas as pd
 import numpy as np
 import sys
 from sklearn import ensemble
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 #splitting training and test sets
-combine= pd.read_csv("combine.csv")
+combine = pd.read_csv("combine.csv")
 train = combine.loc[combine['Id'].isin(pd.read_csv("train.csv")['Id'].tolist())]
+X_train = train.drop(['Id','SalePrice'], axis = 1)
+Y_train = train['SalePrice']
 test = combine.loc[combine['Id'].isin(pd.read_csv("test.csv")['Id'].tolist())]
+Y_test = test.drop(['Id','SalePrice'], axis = 1)
 
+#k-fold validator
+kf = KFold(n_splits = 5, random_state=6969, shuffle=True)
 
-params = { 'n_estimators': 500, 'max_depth':4, 'min_samples_split': 2, 'learning_rate': 0.01, 'loss': 'ls'}
+params = { 'n_estimators': 2000, 'max_depth':4, 'min_samples_split': 2, 'learning_rate': 0.005, 'loss': 'ls', 'subsample':0.8, 'max_features':'sqrt'}
 
 clf = ensemble.GradientBoostingRegressor(**params)
-clf.fit(train.drop(['Id','SalePrice'], axis=1), train['SalePrice'])
 
-res = clf.predict(test.drop(['Id','SalePrice'], axis=1))
+clf.fit(X_train, Y_train)
+
+res = clf.predict(Y_test)
 res = pd.Series(res)
 
 res = pd.concat([test['Id'], res], axis=1)
 res.columns = ['Id', 'SalePrice']
+importances = pd.DataFrame(clf.feature_importances_).T
+importances.columns = X_train.columns
+print(importances)
+
 res.to_csv("res.txt", index=False)
